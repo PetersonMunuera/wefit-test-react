a8ce4a05d871339e44d848a3e570a20b7744ca07
Peterson Munuera <peterson.munuera@somosdx.co>
2022-12-07 21:27:44 -0300

    feat: global style

---
+++ src/styles/globals.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  
+  :root {
+    --light: #fff;
+    --background: #2F2E41;
+    --text: #2F2E41;
+    --text-title: #333;
+    --text-light: #999;
+    --primary: #009EDD;
+    --success: #039B00;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body, input, textarea, button {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+`
