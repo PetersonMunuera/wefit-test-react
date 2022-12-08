50c36106a356f6d90e24d4eba55a62e2bc1a6a80
Peterson Munuera <peterson.munuera@somosdx.co>
2022-12-08 05:36:47 -0300

    fix: _app.jsx refactor

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -1,15 +1,17 @@
+import { TemplateProps } from "./types"
+import { GlobalStyle } from '~/styles/globals'
 import { ToastContainer } from "react-toastify"
 import { Navbar } from "../Navbar"
-import { TemplateProps } from "./types"
 
 import S from "./styles"
 
 export function Template({ children }: TemplateProps) {
   return (
     <S.Container>
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} theme={'dark'} />      
       <Navbar />
       <main>{children}</main>
-      <ToastContainer autoClose={3000} theme={'dark'} />
     </S.Container>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import type { AppProps } from 'next/app'
-
 import { Template } from '../components/Template'
-import { GlobalStyle } from '../styles/globals'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Template>
-      <GlobalStyle />
       <Component {...pageProps} />
     </Template>
   )
