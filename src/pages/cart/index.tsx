04a011239c04a15e741f09983ccfe436fb364cd2
Peterson Munuera <peterson.munuera@somosdx.co>
2022-12-07 23:54:11 -0300

    fix: order complete route

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -4,7 +4,7 @@ export default function Cart() {
   return (
     <>
       <h1>Cart</h1>
-      <Link href='/order/complete'>finalizar pedido</Link>
+      <Link href='/order'>finalizar pedido</Link>
     </>
   )
 }
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function OrderComplete() {
+export default function Order() {
   return (
     <>
       <h1>Order Complete</h1>
